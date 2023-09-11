f8a32f0c7b9f7a523bd8afad6e6bc3264ead1adc
Rini1705 <rinimaulida0517@gmail.com>
2023-09-11 14:34:34 +0700

    login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -7,12 +7,13 @@
 @section('content')
     <section class="section">
         <div class="container mt-5">
+            <div class="row d-flex justify-content-center margin-buttom">
+                <div class="col-lg-10 d-flex">
+                    <img src="{{ asset('assets/img/logo-support.png') }}" alt="logo" class="mx-auto my-auto" width="300">
+                </div>
+            </div>
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                    <div class="login-brand">
-                        <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
-                    </div>
-
                     <div class="card card-primary card-hover rounded">
                         <div class="card-header">
                             <h4>Login</h4>
@@ -45,16 +46,13 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                    <button type="submit" class="btn btn-theme btn-lg btn-block" tabindex="4">
                                         Login
                                     </button>
                                 </div>
                             </form>
 
                         </div>
-                    </div>
-                    <div class="mt-5 text-muted text-center">
-                        Don't have an account? <a href="auth-register.html">Create One</a>
                     </div>
                     <div class="simple-footer">
                         Copyright &copy; MBKM Poliwangi 2023
