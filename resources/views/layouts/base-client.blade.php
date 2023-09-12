6f0ea52d20f441ac4ce63e97de1b2ec284aa0389
unknown <aidaandinar8@gmail.com>
2023-09-12 10:04:37 +0700

    homepagepart1

--- resources/views/layouts/base-client.blade.php
+++ resources/views/layouts/base-client.blade.php
@@ -8,6 +8,11 @@
 
     @yield('title')
 
+    {{-- Font awesome icon cdn --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css') }}">
@@ -58,7 +63,7 @@
             </div>
 
             {{-- footer --}}
-            {{-- @include('') --}}
+            @include('layouts.partials.client.footer-client')
 
         </div>
     </div>
