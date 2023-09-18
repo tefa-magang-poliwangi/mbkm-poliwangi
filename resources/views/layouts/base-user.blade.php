19cd4fbf24c662fb59fab08090cc4866168c6754
yogapangestu0746 <yogapangestu0746@gmail.com>
2023-09-18 14:13:33 +0700

    memperbaiki tampilan doswal

--- resources/views/layouts/base-user.blade.php
+++ resources/views/layouts/base-user.blade.php
@@ -4,8 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+     <link rel="icon" type="image/x-icon" href="{{ asset('images/logo/logo-title-poliwangi.png') }}" />
 
     @yield('title')
+
+    {{-- Font awesome icon cdn --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
