9c4657e193aafd7cd004679cd4aaa3407288750c
yogapangestu0746 <yogapangestu0746@gmail.com>
2023-09-25 09:28:17 +0700

    memperbaiki sirdbar

--- resources/views/layouts/partials/admin/sidebar-admin.blade.php
+++ resources/views/layouts/partials/admin/sidebar-admin.blade.php
@@ -15,7 +15,7 @@
 
             <li class="menu-header">MANAGEMENT ROLE PERMISSION</li>
             <li class="dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-calendar-alt"></i>
+                <a href="#" class="nav-link has-dropdown"><i class="fa-solid fa-bars-progress"></i>
                     <span>Management</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="components-chat-box.html">Role</a></li>
@@ -23,5 +23,61 @@
                     <li><a class="nav-link" href="components-gallery.html">User</a></li>
                 </ul>
             </li>
+
+            <li class="menu-header">DOSEN PEMBIMBING</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fa-solid fa-book"></i>
+                    <span>Laporan Magang</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="components-chat-box.html">Laporan Harian</a></li>
+                    <li><a class="nav-link" href="components-gallery.html">Laporan Mingguan</a></li>
+                    <li><a class="nav-link" href="components-gallery.html">Laporan Akhir</a></li>
+                </ul>
+            </li>
+
+            <li class="menu-header">DOSEN WALI</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link mb-3"><i class="fa-solid fa-list-check"></i>
+                    <span>Kelayakan Pendaftaran MBKM</span></a>
+            </li>
+
+            <li class="menu-header">KAPRODI</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link"><i class="fas fa-info-circle"></i>
+                    <span>Rincian Kegiatan</span></a>
+                <a href="#" class="nav-link"><i class="fas fa-exchange-alt"></i>
+                    <span>Transkrip Nilai</span></a>
+            </li>
+
+            <li class="menu-header">MITRA</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link"><i class="fa-regular fa-building"></i>
+                    <span>Form Mitra</span></a>
+                <a href="#" class="nav-link"><i class="fas fa-user"></i>
+                    <span>Daftar Pelamar</span></a>
+            </li>
+
+            <li class="menu-header">MAHASISWA | LOWONGAN MAGANG</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link"><i class="fas fa-code"></i>
+                    <span>Program</span></a>
+
+            <li class="menu-header">KEGIATANKU MBKM</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-tasks"></i>
+                    <span>Kegiatanku</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="components-chat-box.html">Laporan Harian</a></li>
+                    <li><a class="nav-link" href="components-gallery.html">Laporan Mingguan</a></li>
+                    <li><a class="nav-link" href="components-gallery.html">Laporan Akhir</a></li>
+                </ul>
+                <a href="#" class="nav-link"><i class="fas fa-exchange-alt"></i>
+                    <span>Transkrip Nilai</span></a>
+            </li>
+
+            <li class="menu-header">Akun</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link"><i class="fa-solid fa-user"></i>
+                    <span>Profil</span></a>
     </aside>
 </div>
