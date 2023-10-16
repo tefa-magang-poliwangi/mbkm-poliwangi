0b12efe88100179736156d112e781af35f23f697
Rexcy45 <rembocyber@gmail.com>
2023-10-16 10:09:56 +0700

    import controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,6 +11,9 @@ use Illuminate\Validation\Rules;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Hash;
 use RealRashid\SweetAlert\Facades\Alert;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\UserImport;
+use Illuminate\Support\Facades\Storage;
 
 class UsersController extends Controller
 {
@@ -130,5 +133,36 @@ class UsersController extends Controller
         Alert::success('Success', 'User deleted successfully');
 
         return redirect()->route('users.index');
+    }
+
+    public function import(Request $request)
+    {
+        $this->validate($request, [
+            'file' => 'required|mimes:csv,xls,xlsx'
+        ]);
+
+        $file = $request->file('file');
+
+        // membuat nama file unik
+        $nama_file = $file->hashName();
+
+        //temporary file
+        $path = $file->storeAs('public/excel/', $nama_file);
+
+        // import data
+        $import = Excel::import(new UserImport(), storage_path('app/public/excel/' . $nama_file));
+
+        //remove from server
+        Storage::delete($path);
+
+        if ($import) {
+            //redirect
+            Alert::success('Success', 'Data Berhasil Di Import');
+            return redirect()->back();
+        } else {
+            //redirect
+            Alert::error('Error', 'Data Berhasil Di Import');
+            return redirect()->back();
+        }
     }
 }
