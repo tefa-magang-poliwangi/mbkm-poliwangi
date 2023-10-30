dd0aaff1490ee67b634c998f2e45aff58d12d3f4
akuutauf <taufikhidayatbwx09121@gmail.com>
2023-10-30 14:27:44 +0700

    Memperbaiki Bug Update Role User Super Admin

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -112,12 +112,27 @@ class UsersController extends Controller
     {
         $user->update($request->validated());
 
-        $user->syncRoles($request->get('role'));
+        // Dapatkan ID peran yang dipilih dari permintaan
+        $roleId = $request->get('role');
+
+        if ($roleId) {
+            // Cari nama peran berdasarkan ID
+            $role = Role::find($roleId);
+
+            if ($role) {
+                // Setel peran pengguna dengan nama peran
+                $user->syncRoles($role->name);
+            }
+        } else {
+            // Jika ID peran tidak ada, Anda dapat menghapus peran dari pengguna
+            $user->syncRoles([]);
+        }
 
         Alert::success('Success', 'User updated successfully');
 
         return redirect()->route('users.index');
     }
+
 
     /**
      * Delete user data
@@ -133,36 +148,5 @@ class UsersController extends Controller
         Alert::success('Success', 'User deleted successfully');
 
         return redirect()->route('users.index');
-    }
-
-    public function import(Request $request)
-    {
-        $this->validate($request, [
-            'file' => 'required|mimes:csv,xls,xlsx'
-        ]);
-
-        $file = $request->file('file');
-
-        // membuat nama file unik
-        $nama_file = $file->hashName();
-
-        //temporary file
-        $path = $file->storeAs('public/excel/', $nama_file);
-
-        // import data
-        $import = Excel::import(new UserImport(), storage_path('app/public/excel/' . $nama_file));
-
-        //remove from server
-        Storage::delete($path);
-
-        if ($import) {
-            //redirect
-            Alert::success('Success', 'Data Berhasil Di Import');
-            return redirect()->back();
-        } else {
-            //redirect
-            Alert::error('Error', 'Data Berhasil Di Import');
-            return redirect()->back();
-        }
     }
 }
